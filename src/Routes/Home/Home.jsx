6c353477bba3cf4ef3fa01e96acc694b32ca93db
RyanPeters94 <ryan.ben.peters@gmail.com>
2022-07-16 11:36:12 -0700

    added a bg color to home

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './Home.css'
+import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
-    <main className='home-container'>
-      <h1>Home</h1>
-
+    <main className="home-container">
+      <h1 background-color="blue">Home</h1>
     </main>
   );
 }
