672854f40002d588127e8377e080a785d3e46bca
ryan <ryan.ben.peters@gmail.com>
2022-07-16 11:42:20 -0700

    added legends

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="home-container">
       <h1 background-color="blue">Home</h1>
+      <h2>Were freakin legends</h2>
     </main>
   );
 }
