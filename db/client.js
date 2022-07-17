cdfa92a158d12b80e2dd303b5f716813194daf90
Henry <henryqhuynh@gmail.com>
2022-07-17 15:41:30 -0700

    My basic set-up for our DB, we don't have to use this

---
+++ db/client.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const CONNECTION_STRING = process.env.DATABASE_URL || 'https://localhost:5432/ramh';
+
+const client = new Pool({
+  connectionString: CONNECTION_STRING,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
+
+module.exports = client;
