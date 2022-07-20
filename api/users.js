15dc55a777bc37f1499e7e17d68dda381d69c699
Henry <henryqhuynh@gmail.com>
2022-07-19 20:38:00 -0700

    Adding in some more api/users.js functions

---
+++ api/users.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+const {
+    createUser,
+    getUserByUsername,
+    getUserById,
+    getUser,
+    // getAllUsers
+} = require("../db/users");
+
+const {
+    UserTakenError,
+    PasswordTooShortError,
+    UnauthorizedError,
+} = require("../errors");
+
+// POST /api/users/login
+router.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        const user = await getUser({ username, password });
+        if (!user) {
+            next({
+                name: "Authorization Error: Password",
+                message: "Incorrect password. Please try again.",
+            });
+        } else {
+            const token = jwt.sign(
+                { id: user.id, username: user.username },
+                JWT_SECRET
+            );
+            res.send({
+                token,
+                user,
+                message: "you're logged in!",
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+});
+
+// POST /api/users/register
+router.post("/register", async (req, res, next) => {
+    const { username, password } = req.body;
+    try {
+        const existingUser = await getUserByUsername(username);
+        if (existingUser.length) {
+            next({
+                name: "That username is taken",
+                message: UserTakenError(username),
+            });
+        } else if (password.length < 8) {
+            next({
+                message: PasswordTooShortError(),
+                name: "Password needs to be 8 or more characters!",
+            });
+        } else {
+            const newUser = await createUser({ username, password });
+            const token = jwt.sign(
+                { id: newUser.id, username: newUser.username },
+                JWT_SECRET
+            );
+            res.send({
+                message: "New user created!",
+                token,
+                user: {
+                    id: newUser.id,
+                    username: newUser.username,
+                },
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+});
