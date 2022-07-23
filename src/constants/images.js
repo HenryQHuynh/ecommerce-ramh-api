9aab04bb99b68765ef03298de4a1916c6c878735
Aandrea <aandreab8@gmail.com>
2022-07-23 12:50:43 -0700

    updated carousel

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,5 +1,5 @@
 import background from '../assets/comics.jpg'
-import backgroundtwo from '../assets/superhero.jpeg'
+import backgroundtwo from '../assets/superhero.jpg'
 import newsletter from '../assets/newsletter.jpeg'
 export default {
     background,
