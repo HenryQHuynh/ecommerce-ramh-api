6ff87e660d61ee575c15b3136904bb37b35a7351
Henry <henryqhuynh@gmail.com>
2022-07-24 22:06:15 -0700

    Flushed out CREATE TABLES

--- db/seedData.js
+++ db/seedData.js
@@ -13,6 +13,8 @@ async function dropTables() {
         DROP TABLE IF EXISTS reviews;
         DROP TABLE IF EXISTS orders;
         DROP TABLE IF EXISTS products; 
+        DROP TABLE IF EXISTS authors;
+        DROP TABLE IF EXISTS categories;
         DROP TABLE IF EXISTS users;
         `);
         console.log("Hey man, we did the thing.")
@@ -32,10 +34,21 @@ async function dropTables() {
           username VARCHAR(255) UNIQUE NOT NULL,
           password VARCHAR(255) NOT NULL
         );
-        CREATE TABLE products (
+        CREATE TABLE categories (
+          distId SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL
+        );
+        CREATE TABLE authors (
           id SERIAL PRIMARY KEY,
           name VARCHAR(255) UNIQUE NOT NULL,
           description TEXT NOT NULL
+        );
+        CREATE TABLE products (
+          id SERIAL PRIMARY KEY,
+          title VARCHAR(255) UNIQUE NOT NULL,
+          description TEXT NOT NULL,
+          "authorId" INTEGER REFERENCES authors(id),
+          "catId" INTEGER REFERENCES categories(distId)
         );
         CREATE TABLE orders (
           id SERIAL PRIMARY KEY,
