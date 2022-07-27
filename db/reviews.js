7b03357807267104a10828968248a443740dfd54
Henry <henryqhuynh@gmail.com>
2022-07-26 19:17:30 -0700

    Adding in carts.js to both db and api

--- db/reviews.js
+++ db/reviews.js
@@ -10,7 +10,7 @@ async function getAllReviews() {
         );
         return rows;
     } catch (error) {
-    console.log("Error: Problem retrieving all reviews!...", error);
+    console.error("Error: Problem retrieving all reviews!...", error);
     }
 }
 
@@ -26,11 +26,69 @@ async function getReviewsById(id) {
         console.log(review)
         return review;
     } catch (error) {
-    console.log("Error: Problem getting reviews  by Id...", error);
+    console.error("Error: Problem getting reviews  by Id...", error);
     }    
+}
+
+async function getReviewsByName(name) {
+    try {
+        const {
+            rows: [review],
+        } = await client.query(
+            `
+            SELECT * FROM reviews
+            WHERE name = $1;
+            `, [name]);
+            return review;
+    } catch (error) {
+        console.error("Error: Problem getting reviews by name...", error);
+    }
+}
+
+async function createReviews({ name, description }) {
+    try {
+        const {
+            rows: [review],
+        } = await client.query(
+            `
+            INSERT INTO reviews (name, description )
+            VALUES ($1,$2)
+            ON CONFLICT 
+            `,
+            [name, description]
+        );
+        return review
+    } catch (error) {
+    console.error("Error: Problem creating reviews...", error)
+    }
+}
+
+async function destroyReviews(id) {
+    try {
+        await client.query(
+            `
+            DELETE FROM reviews
+            WHERE "id"=${id};
+            `);
+            const {
+                rows: [review],
+              } = await client.query(
+                `
+                DELETE FROM reviews
+                WHERE id=${id}
+                RETURNING *;
+                `
+              );
+        console.log(review, "Finished deleting review from list!")
+    } catch (error) {
+        console.error("Problem deleting reviews...", error)
+    }
 }
 
 module.exports = {
     getAllReviews,
-    getReviewsById
+    getReviewsById,
+    getReviewsByName,
+    createReviews,
+    destroyReviews
 }
