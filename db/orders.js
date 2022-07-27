f3e0c503858101ccf3d227c995d3d0d86b7d9154
Henry <henryqhuynh@gmail.com>
2022-07-26 21:08:44 -0700

    Setting up orders first before doing cart... Seems like the right idea

--- db/orders.js
+++ db/orders.js
@@ -1,8 +1,26 @@
 const client = require("./client");
 
+// reference routine_activities in fitness tracker
 // DATABASE FUNCTIONS
-
+async function createOrder({ name, productId, userId }) {
+    try {
+        const {
+            rows: [order],
+        } = await client.query(
+        `
+        INSERT INTO orders()
+        VALUES ($1, $2, $3)
+        ON CONFLICT (name, userId) DO NOTHING
+        RETURNING *; 
+        `,
+        [ name, productId, userId ]
+        );
+        return order;
+    } catch (error) {
+        console.error("Error: Problem creating an order list!...", error)
+    }
+}
 
 module.exports = {
-    
+    createOrder
 };
