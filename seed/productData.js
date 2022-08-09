e2177d09b52a5b4fb5a5ee2fd8c271ad891ad2cd
MaxMoir <kooksaga@gmail.com>
2022-08-08 18:49:43 -0700

    first comic push

--- seed/productData.js
+++ seed/productData.js
@@ -1,40 +1,46 @@
 const productsData = [
     {
-        title: "Comic1",
-        author: "Guy1",
+        title: "Venom vs.Carnage",
         publisher: "Marvel",
-        imageLink: "http://placeimg.com/640/480/tech",
+        imageLink: "https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&set=key%5Bresolve.format%5D,value%5Bwebp%5D&source=url%5Bhttps://prodimage.images-bn.com/pimages/9781302928476_p0_v1_s600x595.jpg%5D&scale=options%5Blimit%5D,size%5B550x10000%5D&sink=format%5Bwebp%5D",
+        description:`What's worse than one mayhem-producing symbiote? Two. What's worse than that? Three. That's right: Venom's offspring, Carnage, is about to have a baby itself - a creature of indescribable power and appetites. Question is, who's gonna bring up the baby? Quick - someone call Spider-Man! Guest starring Black Cat! Collecting VENOM VS. CARNAGE (2004) #1-4.`,
+        price:12.99
         
     },
     {
-        title: "Comic2",
-        author: "Lady1",
+        title: ,
         publisher: "DC",
         imageLink: "http://placeimg.com/640/480/tech",
+        description:,
+        price
     },
     {
         title: "Comic3",
-        author: "Guy2",
         publisher: "Marvel",
         imageLink: "http://placeimg.com/640/480/tech",
+        description,
+        price,
     },
     {
         title: "Comic4",
-        author: "Lady2",
         publisher: "DC",
         imageLink: "http://placeimg.com/640/480/tech",
+        description,
+        price,
     },
     {
         title: "Comic5",
-        author: "Guy1",
         publisher: "Marvel",
         imageLink: "http://placeimg.com/640/480/tech",
+        description,
+        price,
     },
     {
         title: "Comic6",
-        author: "Lady3",
         publisher: "DC",
         imageLink: "http://placeimg.com/640/480/tech",
+        description,
+        price,
     },
 
 ]
